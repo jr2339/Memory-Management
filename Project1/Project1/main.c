aefae443462773a045ce9ec583b6532dcdfdd1df
jr2339 <jr2339@nau.edu>
2017-04-15 13:10:06 -0700

    Init

---
+++ Project1/Project1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project1
+//
+//  Created by jr2339 on 4/15/17.
+//  Copyright © 2017 jr2339. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
