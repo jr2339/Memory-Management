280ee74be471280761e53fa6ba6f0e83645fb34e
jr2339 <jr2339@nau.edu>
2017-04-16 03:32:51 -0700

    Start Programming

--- Project1/Project1/Pointer.cpp
+++ Project1/Project1/Pointer.cpp
@@ -8,9 +8,19 @@
 
 #include "Pointer.hpp"
 
-// It 
 
-#define Page_Number 3
-#define Sequence_Number 2
+Page::Page(int page_number){
+    
+    
+    //Allocate the memory for our page
+    int total = pow(256, page_number);
+    memory = malloc(sizeof(char)*total);
+}
+
+
+Page::~Page(){
+    free(memory);
+}
+
 
 
--- Project1/Project1/Pointer.hpp
+++ Project1/Project1/Pointer.hpp
@@ -10,5 +10,21 @@
 #define Pointer_hpp
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
 
+//tring to allocate memory by using C language, it looks lije to difficult
+//So, I am tring to use C++ to solve it
+class Page{
+public:
+    Page(int page_number);
+    ~Page();
+    
+    
+private:
+   void* memory;
+    unsigned int max_count;
+    unsigned int current_poistion;
+    
+};
 #endif /* Pointer_hpp */
