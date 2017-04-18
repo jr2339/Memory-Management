6cfc80566fb463cd81fb3909ead9c688a1a22429
jr2339 <jr2339@nau.edu>
2017-04-18 14:34:44 -0700

    Starting using index

--- Project1/Project1/Pointer.hpp
+++ Project1/Project1/Pointer.hpp
@@ -11,50 +11,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <vector>
-#include <cmath>
-#include <iostream>
-//tring to allocate memory by using C language, it looks lije to difficult
-//So, I am tring to use C++ to solve it
-class Page{
-public:
-    Page(unsigned int size,unsigned int max,uint64_t addr);
-    ~Page();
-    
-    bool is_full();
-    uint64_t get_next_address();
-    
-    
-private:
-    void* memory;
-    unsigned int struct_size;
-    unsigned int max_count;
-    unsigned int current_poistion;
-    uint64_t root_pointer_address; // unit64_t means this variable at here is 8 bytes
-    
-};
-
-
-class MemoryAllocator{
-public:
-    MemoryAllocator(int size);
-    ~MemoryAllocator();
-    
-    uint64_t allocate();
-    void* reference(uint64_t virtual_pointer);
-    
-private:
-    unsigned int struct_size;
-    unsigned int per_page_size;
-    uint64_t next_page_address;
-    
-    std::vector<Page*> pages;
-    
-    Page* addPage();
-    unsigned int get_page_size(unsigned int struct_size);
-   
-};
-
 
 
 #endif /* Pointer_hpp */
