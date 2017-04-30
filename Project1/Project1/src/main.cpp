a791d091089978caf1fd819578a6ba546db1007b
AustinSanders <as2544@nau.edu>
2017-04-29 20:41:22 -0700

    Split prefix_trie.cpp into custom and os versions.

--- Project1/Project1/src/main.cpp
+++ Project1/Project1/src/main.cpp
@@ -13,8 +13,6 @@
 
 
 int main(int argc, const char * argv[]) {
-    
-    
     if(argc < 2){
         perror("We need 2 arguments");
         exit(1);//if the nunber is not 0, not access to error
@@ -23,31 +21,24 @@ int main(int argc, const char * argv[]) {
         perror("We only need 2 arguments");
         exit(1);//if the nunber is not 0, not access to error
     }
-    
      //input stream for file handle
     ifstream ifs;
     // opening input FASTA file
     ifs.open (argv[1], ifstream::in);
-    
     MemoryAllocator *memory;
     memory = new MemoryAllocator(sizeof(pointer));
     Trie* prefix = new Trie();
-    
     //record to add in to the Node
     record new_reord;
     char c;
-    
     while (ifs.get(c)) {
         string header;
         string sequence;
         getline(ifs, header);
         getline(ifs, sequence);
-        
         record new_record = {header,sequence};
         //need to figure out how to add DNA Sequence to our Trie
         //prefix->addWord(new_reord.sequence, sizeof(sequence));
-        
     }
-    
     return 0;
 }
