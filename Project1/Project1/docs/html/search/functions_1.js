0018aa7bf1dc01d85a5a5102d32925e92bcc1849
AustinSanders <as2544@nau.edu>
2017-04-30 14:45:11 -0700

    Finished documentation, forgot to add last time

--- Project1/Project1/docs/html/search/functions_1.js
+++ Project1/Project1/docs/html/search/functions_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['get_5fmemory_5fof',['get_memory_of',['../class_page.html#af540699e2f459eccb3a4991c0b509bce',1,'Page']]],
   ['get_5fnext_5faddress',['get_next_address',['../class_page.html#ae6ac153d53ddb0493a4fff270dfa42cb',1,'Page']]],
   ['getchild',['getChild',['../class_node.html#a7d2c9441ae9f243e06d9f081bada8bad',1,'Node']]],
   ['getrootaddress',['getRootAddress',['../class_page.html#a438a928e0ce67dc404f183678eb533ec',1,'Page']]],
